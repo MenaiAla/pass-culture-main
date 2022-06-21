3476dd696ba12dc33dbaf6cb64f2d2fb8fcc5529
Jeremie Baudet <jeremie.baudet@passculture.app>
2022-06-22 00:02:06 +0200

    (PC-6877)[API] feat: favorites not booked: big query
    Add the Google Big Query query for favorites not booked.
    It will be used to send notifications.

---
+++ api/src/pcapi/connectors/big_query/queries/favorites_not_booked.py
@@ -0,0 +1,24 @@
+import pydantic
+
+from pcapi import settings
+
+from .base import BaseQuery
+
+
+class FavoritesNotBookedModel(pydantic.BaseModel):
+    userId: int
+    offerName: str
+    bookingId: int
+
+
+class FavoritesNotBooked(BaseQuery):
+    query = f"""
+        SELECT
+            *
+        FROM
+            `{settings.BIG_QUERY_NOTIFICATIONS_TABLE_BASE}.favorite_not_booked`
+        WHERE
+            table_creation_day = CURRENT_DATE()
+    """
+
+    model = FavoritesNotBookedModel
