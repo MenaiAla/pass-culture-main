4b920c1cc02abe3113cbedf97f17726f1e92feaf
apibrac <alexis.pibrac@passculture.app>
2022-10-13 16:36:13 +0200

    Automatic redirect for current_user

--- api/src/pcapi/routes/poc_backoffice/home.py
+++ api/src/pcapi/routes/poc_backoffice/home.py
@@ -1,9 +1,13 @@
 from flask import render_template
 from flask_login import current_user
+from flask import url_for
+import werkzeug
 
 from . import blueprint
 
 
 @blueprint.poc_backoffice_web.route("/", methods=["GET"])
 def home():  # type: ignore
+    if current_user and not current_user.is_anonymous:
+        return werkzeug.utils.redirect(url_for("poc_backoffice_web.search_public_accounts"))
     return render_template("home.html", context={"current_user": current_user})
