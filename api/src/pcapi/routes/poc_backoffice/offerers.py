97d472d997405744059c93c2581cd72ffc3c2b27
apibrac <alexis.pibrac@passculture.app>
2022-10-20 18:34:15 +0200

    First turbo install

--- api/src/pcapi/routes/poc_backoffice/offerers.py
+++ api/src/pcapi/routes/poc_backoffice/offerers.py
@@ -34,70 +34,14 @@ def get_offerer(offerer_id: int):  # type: ignore
 
 
 
-# @blueprint.backoffice_blueprint.route("offerers/<int:offerer_id>", methods=["GET"])
-# @spectree_serialize(
-#     response_model=serialization.OffererBasicInfoResponseModel,
-#     on_success_status=200,
-#     api=blueprint.api,
-# )
-# @perm_utils.permission_required(perm_models.Permissions.READ_PRO_ENTITY)
-# def get_offerer_basic_info(offerer_id: int) -> serialization.OffererBasicInfoResponseModel:
+@blueprint.poc_backoffice_web.route("/pro/offerer/<int:offerer_id>/stats", methods=["GET"])
+@utils.ff_enabled(FeatureToggle.ENABLE_NEW_BACKOFFICE_POC)
+@utils.permission_required(perm_models.Permissions.READ_PUBLIC_ACCOUNT, redirect_to=".unauthorized")
+def get_offerer_stats(offerer_id: int):  # type: ignore
+    total_revenue = offerers_api.get_offerer_total_revenue(offerer_id)
+    offers_stats = offerers_api.get_offerer_offers_stats(offerer_id)
 
-#     offerer_basic_info = offerers_api.get_offerer_basic_info(offerer_id)
-
-#     if not offerer_basic_info:
-#         raise api_errors.ResourceNotFoundError(errors={"offerer_id": "La structure n'existe pas"})
-
-#     return serialization.OffererBasicInfoResponseModel(
-#         data=serialization.OffererBasicInfo(
-#             id=offerer_basic_info.id,
-#             name=offerer_basic_info.name,
-#             isActive=offerer_basic_info.isActive,
-#             siren=offerer_basic_info.siren,
-#             region=regions_utils.get_region_name_from_postal_code(offerer_basic_info.postalCode),
-#             bankInformationStatus=serialization.OffererBankInformationStatus(
-#                 **{stat: (offerer_basic_info.bank_informations or {}).get(stat, 0) for stat in ("ko", "ok")}
-#             ),
-#             isCollectiveEligible=offerer_basic_info.is_collective_eligible,
-#         ),
-#     )
-
-
-# @blueprint.backoffice_blueprint.route("offerers/<int:offerer_id>/total_revenue", methods=["GET"])
-# @spectree_serialize(
-#     response_model=serialization.OffererTotalRevenueResponseModel,
-#     on_success_status=200,
-#     api=blueprint.api,
-# )
-# @perm_utils.permission_required(perm_models.Permissions.READ_PRO_ENTITY)
-# def get_offerer_total_revenue(offerer_id: int) -> serialization.OffererTotalRevenueResponseModel:
-#     total_revenue = offerers_api.get_offerer_total_revenue(offerer_id)
-
-#     return serialization.OffererTotalRevenueResponseModel(data=total_revenue)
-
-
-# @blueprint.backoffice_blueprint.route("offerers/<int:offerer_id>/offers_stats", methods=["GET"])
-# @spectree_serialize(
-#     response_model=serialization.OffererOfferStatsResponseModel,
-#     on_success_status=200,
-#     api=blueprint.api,
-# )
-# @perm_utils.permission_required(perm_models.Permissions.READ_PRO_ENTITY)
-# def get_offerer_offers_stats(offerer_id: int) -> serialization.OffererOfferStatsResponseModel:
-#     # TODO: réduire de le timeout de requête SQL pour ce endpoint
-#     #  (peu d'intérêt pour des grosses structures pour qui le requête va prendre
-#     #  de toute façon trop de temps, alors autant ne pas bourriner la DB pour rien)
-#     offers_stats = offerers_api.get_offerer_offers_stats(offerer_id)
-
-#     return serialization.OffererOfferStatsResponseModel(
-#         data=serialization.OffersStats(
-#             active=serialization.BaseOffersStats(
-#                 individual=offers_stats.individual_offers["active"] if offers_stats.individual_offers else 0,
-#                 collective=offers_stats.collective_offers["active"] if offers_stats.collective_offers else 0,
-#             ),
-#             inactive=serialization.BaseOffersStats(
-#                 individual=offers_stats.individual_offers["inactive"] if offers_stats.individual_offers else 0,
-#                 collective=offers_stats.collective_offers["inactive"] if offers_stats.collective_offers else 0,
-#             ),
-#         )
-#     )
+    return render_template("offerer/get/stats.html",
+        total_revenue=total_revenue,
+        offers_stats=offers_stats
+    )
